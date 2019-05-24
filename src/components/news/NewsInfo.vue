ec35e57c4331259f86e57563827f9bd799a61086
zpj80231 <916665067@qq.com>
2019-05-24 21:21:23 +0800

    完成新闻详情中的 提交评论 功能

--- src/components/news/NewsInfo.vue
+++ src/components/news/NewsInfo.vue
@@ -4,66 +4,76 @@
         <h3 class="title">{{ newsinfo.title }}</h3>
         <!-- 子标题 -->
         <p class="subtitle">
-          <span>发表时间：{{ newsinfo.add_time | dateFormat }}</span>
-          <span>点击：{{ newsinfo.click }}次</span>
+            <span>发表时间：{{ newsinfo.add_time | dateFormat }}</span>
+            <span>点击：{{ newsinfo.click }}次</span>
         </p>
         <hr>
         <!-- 内容区域 -->
         <div class="content" v-html="newsinfo.content"></div>
-        <!-- 评论区域：  导入评论子组件 -->
-
+        <!--
+            评论区域：  引入评论子组件
+            :id为 详情组件向评论子组件传值
+         -->
+        <comment :id="this.id"></comment>
     </div>
 </template>
 
 <script type="text/javascript">
-//import {Toast} from ""
-export default{
-    data() {
-        return{
-            id: this.$route.params.id,
-            newsinfo: {}
-        }
-    },
-    created() {
-        this.getNewsInfo();
-    },
-    methods: {
-        getNewsInfo() {//获取新闻详情
-            this.$http.get("api/getnew/"+this.id).then(result => {
-                if(result.body.status === 0){
-                    this.newsinfo = result.body.message[0];
-                }else {
-                    Toast("获取新闻详情失败");
-                }
-            })
+    // 1. 导入评论子组件
+    import comment from "../subcomponents/comment.vue"
+
+    export default{
+        data() {
+            return {
+                id: this.$route.params.id,
+                newsinfo: {}
+            }
+        },
+        created() {
+            this.getNewsInfo();
+        },
+        methods: {
+            getNewsInfo() {//获取新闻详情
+                this.$http.get("api/getnew/" + this.id).then(result => {
+                    if (result.body.status === 0) {
+                        this.newsinfo = result.body.message[0];
+                    } else {
+                        Toast("获取新闻详情失败");
+                    }
+                })
+            }
+        },
+        components: {
+            //2. 子组件能已 html标签 形式 在模板中显示
+            "comment": comment
         }
     }
-}
 </script>
 /*
   我的理解就是设置为 两端对齐
   display: flex;
   justify-content: space-between;
 */
+
 <style lang="scss">
-.newsinfo-container {
-  padding: 0 4px;
-  .title {
-    font-size: 16px;
-    text-align: center;
-    margin: 15px 0;
-    color: red;
-  }
-  .subtitle {
-    font-size: 13px;
-    color: #226aff;
-    display: flex;
-    justify-content: space-between;
-  }
-  .content {
-    img {
-      width: 100%;
+    .newsinfo-container {
+        padding: 0 4px;
+        .title {
+            font-size: 16px;
+            text-align: center;
+            margin: 15px 0;
+            color: red;
+        }
+        .subtitle {
+            font-size: 13px;
+            color: #226aff;
+            display: flex;
+            justify-content: space-between;
+        }
+        .content {
+            img {
+                width: 100%;
+            }
+        }
     }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,10 +7,9 @@ import app from "./App.vue"
 // 导入格式化插件
 import moment from "moment"
 // 定义Vue全局过滤器 (时间转换过滤器)
-Vue.filter('dateFormat',function(dataStr, pattern = "YYYY-MM-DD HH:mm:ss"){
+Vue.filter('dateFormat', function (dataStr, pattern = "YYYY-MM-DD HH:mm:ss") {
     return moment(dataStr).format(pattern)
 })
-
 
 
 // 1.1 导入路由的包
@@ -24,13 +23,17 @@ import VueResource from 'vue-resource'
 Vue.use(VueResource)
 // 设置请求的根路径
 Vue.http.options.root = 'http://www.liulongbin.top:3005/';
+// 全局设置 post 时候表单数据格式组织形式(省得每次手写了)   application/x-www-form-urlencoded
+Vue.http.options.emulateJSON = true;
+
 
 
 // 按需导入 Mint-UI 中的组件
-import { Header,Swipe, SwipeItem  } from 'mint-ui'
+import {Header, Swipe, SwipeItem, Button} from 'mint-ui'
 Vue.component(Header.name, Header)//显示顶部区域
 Vue.component(Swipe.name, Swipe);//展示首页轮播图组件
 Vue.component(SwipeItem.name, SwipeItem);//展示首页轮播图组件
+Vue.component(Button.name, Button);//展示按钮
 
 // 导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
